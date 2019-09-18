13472cff5c84f2ad380c6c417f4bad8fc9ce888b
towasme <towasm@gmail.com>
2019-09-18 22:10:43 +0000

    ans

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.sh>
+/**
+ *
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{printf("Size of a char: ",sizeof (char), "byte(s)");
+printf("Size of an int: ",sizeof (int), "byte(s)");
+printf("Size of a long: ",sizeof (long int), "byte(s)");
+printf("Size of a long long int: ",sizeof (long long int), "byte(s)");
+printf("Size of a float: ",sizeof (float), "byte(s)");
+return (0);
+}
