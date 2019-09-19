6dfc4bd5d229a3d736f14b80d316f0a354efdb7a
towasme <towasm@gmail.com>
2019-09-19 19:06:35 +0000

    ans

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,14 +10,14 @@ n = rand() - RAND_MAX / 2;
 last = n % 10;
 if (n > 5)
 {
-printf("%d and is greater than 5\n", last);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
 else if (n == 0)
 {
-printf("%d and is 0\n", last);
+printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else
-{printf("%d and is less than 6 and not 0\n", last);
+{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 return (0);
 }
