221296ba42eef96dcdd50ed1aba48319ea290c96
towasme <towasm@gmail.com>
2019-09-19 22:03:13 +0000

    ans

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+int CH;
+for (ch = '0'; ch <= '8' ; ch++)
+{
+putchar(CH);
+}
+{
+putchar(', ')
+}
+{
+putchar(9)
+}
+putchar('\n');
+return (0);
+}
