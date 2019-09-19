4a7aef0e5cdedb370cd2efce61b8ce7d0adc9e8a
towasme <towasm@gmail.com>
2019-09-19 20:08:32 +0000

    ans

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+int CH;
+for (ch = 'a'; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (CH = 'A'; ch <= 'Z' ; ch++)
+{
+putchar(CH);
+}
+putchar('\n');
+return (0);
+}
