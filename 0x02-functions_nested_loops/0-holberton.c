de1125d5d7be1eb00e78b329e11ee80b9cfdaca2
towasme <towasm@gmail.com>
2019-09-23 21:19:35 +0000

    ans

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+int main(void)
+{
+int i;
+char a[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0'};
+for (i = 0; i <= 9; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+return(0);
+}
