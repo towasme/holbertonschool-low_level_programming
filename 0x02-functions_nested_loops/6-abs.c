5f8f058743f424b4913aebf6539ba760b0bd4910
towasme <towasm@gmail.com>
2019-09-24 18:54:29 +0000

    ans

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_abs - computes the absolute value of an integer
+ *@c: variable to be replaced
+ *
+ *Return: Always
+ */
+int _abs(int);
+{
+int r;
+if(r < 0)
+	{
+		r = r*(-1);
+		return (r);
+	} else
+		{return (r);
+}
+}
