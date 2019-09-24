bd8b3bdb8a6672c439d95c31fa4cdfc2bf638ba7
towasme <towasm@gmail.com>
2019-09-24 17:32:01 +0000

    changed expression

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,11 +6,15 @@
  */
 int main(void)
 {
-int i;
-char a[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0'};
-for (i = 0; i <= 9; i++)
-{
-_putchar(a[i]);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n\n');
 }
 _putchar('\n');
 return (0);
