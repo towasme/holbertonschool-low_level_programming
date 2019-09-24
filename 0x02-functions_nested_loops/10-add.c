620ee8f77c3ac244eba934845b06fbe4f4e42e2b
towasme <towasm@gmail.com>
2019-09-24 22:28:53 +0000

    ans

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *add - adds two integers and returns the result
+ *
+ *@a:first variable
+ *@b: second variable
+ *Return: Always
+ */
+int add(int a, int b)
+{
+	int c = 0;
+
+	c = a + b;
+	return (c);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -19,6 +19,8 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
+int add(int a, int b);
+
 
 
 
