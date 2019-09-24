f6c3ff57400a60811573bcc8ee5ccd2f29570e09
towasme <towasm@gmail.com>
2019-09-24 14:39:03 +0000

    corrected betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,18 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
+ *_islower - Checks for lowercase character
+ *@c: variable to be replaced
  *
- * Return: Always 0.
+ *Return: Always
  */
 int _islower(int c)
-
+{
+if (c >= 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
