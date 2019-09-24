3f2ef38622394697033d1743784633cac1141da0
towasme <towasm@gmail.com>
2019-09-24 18:34:02 +0000

    ans

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_sign - prints the sign of a number
+ *@n: variable to be replaced
+ *
+ *Return: Always
+ */
+int print_sign(int n)
+	{
+		if(n > 0)
+		{_putchar('+');
+		return (1);
+		}
+		else if(n == 0)
+		{_putchar('0');
+		return (0);
+		}
+		else
+		{_putchar('-');
+		return(-1);
+		}
+}
