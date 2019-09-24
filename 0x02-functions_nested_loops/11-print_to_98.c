4fde583369a61b56e6bda255df69f3ef64a39e91
towasme <towasm@gmail.com>
2019-09-24 23:18:14 +0000

    pre ans

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *jack_bauer - prints the sign of a number
+ *
+ *Return: Always
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+	{prinf("%d", n);
+	n++
