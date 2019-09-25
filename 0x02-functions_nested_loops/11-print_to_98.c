d86d85f8406bd6ac7329a35c8d57258f9565515b
towasme <towasm@gmail.com>
2019-09-25 23:22:22 +0000

    corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,47 @@
 #include "holberton.h"
 #include <stdio.h>
-/**
- *jack_bauer - prints the sign of a number
- *
- *Return: Always
- */
+/**¬
+*jack_bauer - prints the sign of a number¬
+*¬
+*Return: Always¬
+*/
 void print_to_98(int n)
 {
-if (n <= 98)
-	{prinf("%d", n);
-	n++
+    if (n == 98)
+        {
+            printf("%d, ", n);
+        }
+      else if (n <= 97)
+         {
+             do 
+            {
+            n++;
+             if(n != 98)
+                { 
+                    printf("%d, ", n);
+                }
+                else
+                {  
+                    printf("%d", n);
+                }
+            }
+        while(n <=97);
+         }
+        else if(n > 98)
+            {
+                do 
+                {
+                n--;
+                if(n != 98)
+                { 
+                    printf("%d, ", n);
+                }
+                else
+                {  
+                    printf("%d", n);
+                }
+                }
+                while(n > 98);
+            }
+    
+}
