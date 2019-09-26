748f35f9bbcd94104b1b4cf2623d009a2b62ad78
towasme <towasm@gmail.com>
2019-09-26 04:31:56 +0000

    change b and c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,11 +9,11 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && b > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b > a && b > c)
 {
 largest = b;
 }
@@ -21,17 +21,10 @@ else if (a == b)
 {
 largest = a;
 }
-else if(b == c)
-{
-largest = b:
-}
-else if(a == c)
-{
-largest = a;
-}
 else
 {
 largest = c;
 }
+
 return (largest);
 }
