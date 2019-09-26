12400f5aeb5a6d2550ee7d7f6b0c1bd2c02ae14b
towasme <towasm@gmail.com>
2019-09-26 18:53:34 +0000

    corrections

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 /**
  *print_numbers - Prints numbers from 0 to 9
  */
-void print_alphabet(void)
+void print_numbers(void);
 {
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
 		{
 		_putchar(i);
 		}
 		_putchar('\n');
-	return;
 }
