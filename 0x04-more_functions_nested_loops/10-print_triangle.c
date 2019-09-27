c848e55c762ba3b1b4b49da84ad49fc1d1294460
towasme <towasm@gmail.com>
2019-09-27 04:35:34 +0000

    ans

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *print_triangle - Prints
+ *@n: variable
+ */
+void print_triangle(int size)
+{
+int b;
+int c;
+int d;
+
+	if (size < 0)
+	_putchar('\n');
+		else
+		{
+		for (b = 0; b < size; b++)
+		{
+			for (c = 0; c < size; c++)
+				{
+				d = size - b - 1;
+						if (c < d)
+						_putchar(' ');
+						else
+						_putchar('#');
+				}
+			_putchar('\n');
+		}
+		}
+}
