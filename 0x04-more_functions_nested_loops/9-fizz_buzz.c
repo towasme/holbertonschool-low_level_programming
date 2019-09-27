ca0dec181a0877b572079610ec74298ebec7d0b4
towasme <towasm@gmail.com>
2019-09-27 20:35:17 +0000

    ans

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int n;
 for (n = 1; n <= 100; n++)
 	{
 	if (n == 100)
-		printf("Buzz");
+		printf("Buzz\n");
 	else if ((n % 3 == 0) && (n % 5 == 0))
 		printf("FizzBuzz ");
 	else if (n % 3 == 0)
