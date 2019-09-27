3e3f7636f236ecf36d6bb059247295ff07df8be7
towasme <towasm@gmail.com>
2019-09-27 03:07:21 +0000

    corrections in order of functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,11 @@ void print_line(int n)
 {
 	int b;
 
-	if (n < 0)
+	if (n <= 0)
 	_putchar('\n');
 	else
 		{
-		for (b = 0; b < n; b++)
+		for (b = 0; b <= n; b++)
 			_putchar('_');
 		}
 	_putchar('\n');
