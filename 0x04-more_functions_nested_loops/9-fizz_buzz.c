7400456c7e525677e7d10225664a0f8b7f10b9e7
towasme <towasm@gmail.com>
2019-09-27 20:25:03 +0000

    ans

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *print_square- Prints
+ *@size: variable
+ */
+int main(void)
+{
+int n;
+
+for (n = 1; n <= 100; n++)
+	{
+	if (n == 100)
+		printf("Buzz");
+	else if ((n % 3 == 0) && (n % 5 == 0))
+		printf("FizzBuzz ");
+	else if (n % 3 == 0)
+		printf("Fizz ");
+	else if (n % 5 == 0)
+		printf("Buzz ");
+	else
+	printf("%d ", n);
+	}
+return(0);
+}
