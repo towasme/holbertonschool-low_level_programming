1b55d8ca9025978b0d744ac0a68df11180a45f66
towasme <towasm@gmail.com>
2019-09-27 04:39:57 +0000

    ans

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  *print_triangle - Prints
- *@n: variable
+ *@size: variable
  */
 void print_triangle(int size)
 {
@@ -9,21 +9,21 @@ int b;
 int c;
 int d;
 
-	if (size < 0)
-	_putchar('\n');
-		else
-		{
-		for (b = 0; b < size; b++)
-		{
-			for (c = 0; c < size; c++)
-				{
-				d = size - b - 1;
-						if (c < d)
-						_putchar(' ');
-						else
-						_putchar('#');
-				}
-			_putchar('\n');
-		}
-		}
+if (size <= 0)
+_putchar('\n');
+	else
+	{
+	for (b = 0; b < size; b++)
+	{
+		for (c = 0; c < size; c++)
+			{
+			d = size - b - 1;
+				if (c < d)
+				_putchar(' ');
+				else
+				_putchar('#');
+			}
+		_putchar('\n');
+	}
+	}
 }
