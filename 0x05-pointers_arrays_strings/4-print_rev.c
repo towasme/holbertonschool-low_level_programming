6d790fb2ccf71485b8e0405b8c0a8b77ad0796c9
towasme <towasm@gmail.com>
2019-10-01 15:57:13 +0000

    ans

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_rev- Prints
+ *@*s: pointer
+ *Return: Always
+ */
+void print_rev(char *s)
+
+{
+int n;
+int c;
+
+	for (n = 0; s[n]; n++)
+		{
+		}
+	for (c = n; s[c] >= 0; c--)
+		{
+		_putchar(s[c]);
+		}
+	_putchar('\n');
+}
