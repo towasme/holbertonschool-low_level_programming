e42f7b6f1c1b361e3a6297f5d74350e92b73b8d3
towasme <towasm@gmail.com>
2019-10-03 04:02:35 +0000

    corrections addded another for

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,19 @@
  */
 void print_array(int *a, int n)
 {
-int b = n;
+int b;
 
-	for (n = 0; a[n + 1] != '\0'; n++)
-	{
-		if (n == (b - 1))
-			printf("%d", a[n]);
-		else
-			printf("%d, ", a[n]);
-	}
-	printf("\n");
+for (n = 0; a[n + 1] != '\0'; n++)
+{
+}
+for (b = 0; b < n; b++)
+{
+if (b == (n - 1))
+{
+printf("%d", a[b]);
+}
+else
+printf("%d, ", a[b]);
+}
+printf("\n");
 }
