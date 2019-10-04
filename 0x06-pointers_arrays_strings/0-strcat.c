1e96f9fa902f28111ff7bc1e3127f00201c7c31d
towasme <towasm@gmail.com>
2019-10-04 02:08:18 +0000

    changed while into a for

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,14 @@ char *_strcat(char *dest, char *src)
 {
 
 int i;
-int j = 0;
+int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		{
 		}
-	while (dest[i] != '\0')
+	for (j = 0; src[j] != '\0'; j++)
 		{
-		dest[i + 1] = src[j];
-		j++;
-		src[j] = '\0';
+		dest [i + j] = src[j]:
 		}
 	return (dest);
 }
