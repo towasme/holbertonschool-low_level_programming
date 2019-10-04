0828e780e5286424e17f3f823908d5766812e6e3
towasme <towasm@gmail.com>
2019-10-04 02:13:52 +0000

    removed a space

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+IInclude "holberton.h"
 /**
  **_strcat - concatenates two strings
  *@dest: pointer string
@@ -13,10 +13,10 @@ int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		{
-		}
 	for (j = 0; src[j] != '\0'; j++)
 		{
 		dest[i + j] = src[j]:
+		}
 		}
 	return (dest);
 }
