c8e9e7251cfe5b571a2fbbaf3e5e737fdc79b8d4
towasme <towasm@gmail.com>
2019-10-04 19:33:07 +0000

    start

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ **cap_string - concatenates two strings
+ *@s: pointer string
+ *Return: Always
+ */
+char *leet(char *s)
+{
+char u[] = "AaEeOoTtLl"
+char i[] = "4433007711"
+
+for(s = 0; s[]
