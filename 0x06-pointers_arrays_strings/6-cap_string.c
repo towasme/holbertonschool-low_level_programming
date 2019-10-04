3e9e5cb6aadf2f8bce080bfe37e77663ed14067b
towasme <towasm@gmail.com>
2019-10-04 04:54:55 +0000

    partial ans

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ **cap_string - concatenates two strings
+ *@s: pointer string
+ *Return: Always
+ */
+char *cap_string(char *s)
+{
+int c = 0;
+
+	while (s[c] != '\0')
+		{
+		/*if (s[c] == 32 || s[c] == 46)
+			{
+			s[c + 1] = s[c + 1] - 32;
+			}*/
+		if ((s[c] >= 33 && s[c] <= 44) || (s[c] == 59) || (s[c] == 63) || (s[c] == 123) || (s[c] == 125))
+		{
+			s[c + 1] = s[c + 1] - 32;
+		}
+		c++;
+		}
+	return (s);
+}
