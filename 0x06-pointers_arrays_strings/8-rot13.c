9ea3357c7469c483cab8ad688f4dc59ed4a73c01
towasme <towasm@gmail.com>
2019-10-06 22:06:40 +0000

    corrections

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,16 +8,17 @@ char *rot13(char *s)
 {
 char u[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char i[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-int a;
-int b;
+int a = 0;
+int b = 0;
 
 	while (s[a] != '\0')
 	{
-		for (b = 0; b <= 52; b++)
+		for (b = 0; b <= 54; b++)
 			{
 			if (s[a] == u[b])
 			{
 			s[a] = i[b];
+			break;
 			}
 			}
 	a++;
