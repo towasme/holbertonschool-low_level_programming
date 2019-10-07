bb1ccefcf0cae4e8eb7736bcc3c066d6ef65c3ed
towasme <towasm@gmail.com>
2019-10-07 04:54:58 +0000

    ans

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,18 @@
 /**
  *main -Entry point
  *
- *Return: Always 0 (Success)
+ *Return: Always
  */
 int main(void)
 {
+int a = 2;
+if (a != 0)
+{
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
+}
+else
+{
+return (0);
+}
 }
