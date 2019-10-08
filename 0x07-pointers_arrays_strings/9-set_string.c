bcce15f36591163c73598aeab7563ccb068a6444
towasme <towasm@gmail.com>
2019-10-08 04:38:18 +0000

    ans

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *set_string - concatenates two strings
+ *@s: pointer to a pointer
+ *@to: pointer destination
+ *Return: Always
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
