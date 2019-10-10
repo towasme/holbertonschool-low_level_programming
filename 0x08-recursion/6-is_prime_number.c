65ca07bd653bc7c526c22f8a20c181923348a0cf
towasme <towasm@gmail.com>
2019-10-10 00:04:31 +0000

    start

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *is_prime_number - prints characters
+ *@n: var
+ *Return: Always
+ */
+int is_prime_number(int n)
+{
+int a = 0;
+
+	if (n <= 1)
+		{
+		return (0);
+		}
+	else if (n >= 2 && n / a == 1)
+		{
+		
+		}
