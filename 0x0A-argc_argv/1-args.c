cecd0126c364978d7cc9ddc5158c2ba68097a68c
towasme <towasm@gmail.com>
2019-10-12 23:39:17 +0000

    ans

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - concatenates two strings
+ *@argc: pointer string
+ *@argv: pointer destination
+ *Return: Always
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
