8eb23c6cc923afeb41a99228bf869dc80694feea
towasme <towasm@gmail.com>
2019-10-12 23:17:59 +0000

    ans

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - concatenates two strings
+ *@argc: pointer string
+ *@argv: pointer destination
+ *Return: Always
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+		{
+		printf("%s\n", *argv);
+		}
+	return (0);
+}
