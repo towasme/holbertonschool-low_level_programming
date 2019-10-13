f88e4b47215182e015ca79f196d6b4004a45b4bc
towasme <towasm@gmail.com>
2019-10-13 03:58:37 +0000

    ans

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - concatenates two strings
+ *@argc: pointer string
+ *@argv: pointer destination
+ *Return: Always
+ */
+int main(int argc, char *argv[])
+{
+int mul;
+int num1;
+int num2;
+
+	if (argc > 1)
+		{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		}
+	else
+		{
+		printf("Error\n");
+		}
+	return (0);
+}
