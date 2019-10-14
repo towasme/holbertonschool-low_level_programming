878962000f2398ab0685c6ff28b1748e5deb3b15
towasme <towasm@gmail.com>
2019-10-14 01:05:39 +0000

    start

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - concatenates two strings
+ *@argc: pointer string
+ *@argv: pointer destination
+ *Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+
+int sum, i, c;
+char *p;
+
+	if (argc < 0)
+		{
+		printf("0/n");
+		return (0);
+		}
+	else if (argc > 0)
+		{
+		for (i = 1; i < argc; i++)
+			{
+			p = argv[i];
+			for (c = 0; p[c]; c++)
+				{
+				if (p[c] <= 47 || p[c] >= 58)
+					{
+					printf("Error\n");
+					return (1);
+					}
+				}
+			sum += atoi(argv[i]);
+			}
+		printf("%d\n", sum);
+		}
+	return (0);
+}#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - concatenates two strings
+ *@argc: pointer string
+ *@argv: pointer destination
+ *Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+
+int sum, i, c;
+char *p;
+
+	if (argc < 0)
+		{
+		printf("0/n");
+		return (0);
+		}
+	else if (argc > 0)
+		{
+		for (i = 1; i < argc; i++)
+			{
+			p = argv[i];
+			for (c = 0; p[c]; c++)
+				{
+				if (p[c] <= 47 || p[c] >= 58)
+					{
+					printf("Error\n");
+					return (1);
+					}
+				}
+			sum += atoi(argv[i]);
+			}
+		printf("%d\n", sum);
+		}
+	return (0);
+}
