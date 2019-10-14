fb759614e4e08249f2c1dc1ff54d74c229fd824d
towasme <towasm@gmail.com>
2019-10-14 01:25:41 +0000

    start

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,7 +18,12 @@ char *p;
 		printf("0/n");
 		return (0);
 		}
-	else if (argc > 0)
+	else if (argc != 1)
+		{
+		printf("Error\n");
+		return (1);
+		}
+	else 
 		{
 		for (i = 1; i < argc; i++)
 			{
@@ -36,42 +41,4 @@ char *p;
 		printf("%d\n", sum);
 		}
 	return (0);
-}#include <stdio.h>
-#include <stdlib.h>
-/**
- *main - concatenates two strings
- *@argc: pointer string
- *@argv: pointer destination
- *Return: Always
- */
 
-int main(int argc, char *argv[])
-{
-
-int sum, i, c;
-char *p;
-
-	if (argc < 0)
-		{
-		printf("0/n");
-		return (0);
-		}
-	else if (argc > 0)
-		{
-		for (i = 1; i < argc; i++)
-			{
-			p = argv[i];
-			for (c = 0; p[c]; c++)
-				{
-				if (p[c] <= 47 || p[c] >= 58)
-					{
-					printf("Error\n");
-					return (1);
-					}
-				}
-			sum += atoi(argv[i]);
-			}
-		printf("%d\n", sum);
-		}
-	return (0);
-}
