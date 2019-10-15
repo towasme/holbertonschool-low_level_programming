9d7ab9982cb6781bfc68848651b7112b7eb1ba62
towasme <towasm@gmail.com>
2019-10-15 19:29:48 +0000

    ans

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strdup - creates an array
+ *@str: positive integer
+ *Return: Always
+ */
+char *_strdup(char *str)
+{
+char *new = NULL;
+int i, b;
+	for (i = 0; str[i] != '\0'; i++)
+		{
+		}
+	new = malloc(i * sizeof(char));
+	if (new == NULL)
+		{
+		return (NULL);
+		}
+	for (b = 0; b <= i; b++)
+		{
+		new[b] = str[b];
+		}
+	return (new);
+}
