2a362342375921fbee89520bb767cc07982abe5e
towasme <towasm@gmail.com>
2019-10-21 22:12:15 +0000

    fixed structure alias

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,13 @@ int _putchar(char c);
  *@owner: var
  *Return: Always
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
