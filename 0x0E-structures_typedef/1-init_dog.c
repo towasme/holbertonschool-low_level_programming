edb34ea61d9eabf2456fbd6465bf2ca9be6f67aa
towasme <towasm@gmail.com>
2019-10-21 21:54:50 +0000

    ans

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *init_dog - creates an array
+ *@name: var typ char
+ *@age: var
+ *@owner: var
+ *@d: var
+ *Return: Always
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
