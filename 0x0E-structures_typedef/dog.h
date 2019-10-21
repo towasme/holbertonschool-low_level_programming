b9f1d7575f91b16b6f429f39d4a1719ddbbed32b
towasme <towasm@gmail.com>
2019-10-21 21:12:01 +0000

    header files and README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+
+
+/**
+ *struct dog - creates an array
+ *@name: var
+ *@age: var
+ *@owner: var
+ *Return: Always
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
