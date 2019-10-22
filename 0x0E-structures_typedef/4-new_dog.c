e1f9dc3e98eefe4a714b9ada3be8e7e22f88e6ea
towasme <towasm@gmail.com>
2019-10-22 22:06:15 +0000

    ans

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,24 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- *free_dog - creates an array
- *@d: pointer
+ *new_dog - new struct made
+ *@name: pointer
+ *@age: float var
+ *@owner: char type var
  *Return: Always
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-	free(d->name);
-	free(d->owner);
-	free(d);
-	}
-}
+	dog_t *dog;
+	int i = 0, j = 0;
+	
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (dog->name == NULL)
+		return (NULL);
+	while (name)
+		dog->name[i] = name[i];
+
