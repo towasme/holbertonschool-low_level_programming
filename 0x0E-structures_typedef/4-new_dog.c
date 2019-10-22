0c58c12c60d9342c66739b28236d1a044976fd71
towasme <towasm@gmail.com>
2019-10-22 05:30:05 +0000

    start

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *free_dog - creates an array
+ *@d: pointer
+ *Return: Always
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
