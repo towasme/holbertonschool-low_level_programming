633f6a00dd9b0815efa08f42963b8f5e9202cf03
towasme <towasm@gmail.com>
2019-10-23 19:31:04 +0000

    removed standard library

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if ((array) == NULL || (size == 0))
-	{
-		return;
-	}
 	while (i < size)
 	{
 		(*action)(array[i]);
