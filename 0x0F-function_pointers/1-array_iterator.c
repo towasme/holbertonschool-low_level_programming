e39697ef7f943a866d8d14680b3ebf91ad101d27
towasme <towasm@gmail.com>
2019-10-23 19:23:54 +0000

    ans

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - creates an array
+ *@array: pointer
+ *@size: function pointer
+ *@action: function pointer
+ *Return: Always
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if ((array) == NULL || (size == 0) || ((*action) == NULL))
+	{
+		return;
+	}
+	while (i < size)
+	{
+		(*action)(array[i]);
+		i++;
+	}
+}
