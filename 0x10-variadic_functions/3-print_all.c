1e52e44b63a8a6c70d67d7063c72c7d76b5e2334
towasme <towasm@gmail.com>
2019-10-26 00:00:32 +0000

    ans

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *print_string - check the code for Holberton School students.
+ *@n: positive var
+ *@separator: var type char
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, cont = 0;
+	va_list lst;
+
+	va_start(lst, format);
+
+	while (format[cont] != '\0')
+		cont++;
+
+	if (format[i] == '\0')
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+				case 'c': printf("%c", va_arg(lst, int));
+				break;
+				case 'i': printf("%i", va_arg(lst, int));
+				break;
+				case 'd': printf("%f", (float)(va_arg(lst, double)));
+				break;
+				case 's': printf("%s", va_arg(lst, char*));
+				break;
+				default: 
+				break;
+			}
+			if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+ format[i] == 'd' || format[i] == 's'))
+			{
+				printf(", ");
+			}
+			i++;
+		}
+		printf("\n");
+	}
+}
