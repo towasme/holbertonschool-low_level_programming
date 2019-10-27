ce07a703c884ec779ada88f09afbc5e1bd780146
towasme <towasm@gmail.com>
2019-10-27 03:31:18 +0000

    removed variables, change cases

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,41 +9,41 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, cont = 0;
+	int i = 0;
 	va_list lst;
+	char *prt = NULL;
 
 	va_start(lst, format);
-	while (format[cont] != '\0')
-		cont++;
-
-	if (format[i] == '\0')
-		printf("(nil)");
-	else
+	while (format[i] != '\0')
 	{
-		while (format[i] != '\0')
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-				printf("%c", va_arg(lst, int));
+			case 'c':
+			printf("%c", va_arg(lst, int));
+			break;
+			case 'i':
+			printf("%i", va_arg(lst, int));
+			break;
+			case 'd':
+			printf("%f", (float)(va_arg(lst, double)));
+			break;
+			case 's':
+				prt = va_arg(lst, char*);
+				if (prt == NULL)
+				{
+					prt = ("(nil)");
+				}
+				printf("%s", prt);
 				break;
-				case 'i':
-				printf("%i", va_arg(lst, int));
-				break;
-				case 'd':
-				printf("%f", (float)(va_arg(lst, double)));
-				break;
-				case 's':
-				printf("%s", va_arg(lst, char*));
-				break;
-			}
-			if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+			default:
+			break;
+		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
 format[i] == 'd' || format[i] == 's'))
 				printf(", ");
 
-			i++;
-		}
-		printf("\n");
+		i++;
 	}
+	printf("\n");
 	va_end(lst);
 }
