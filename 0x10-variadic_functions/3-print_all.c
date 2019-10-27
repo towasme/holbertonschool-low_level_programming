f65f48caa2cd12720f7892541892a51f45612fa6
towasme <towasm@gmail.com>
2019-10-27 03:07:33 +0000

    fixed betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,9 +3,8 @@
 #include <stdarg.h>
 
 /**
- *print_string - check the code for Holberton School students.
- *@n: positive var
- *@separator: var type char
+ *print_all - check the code for Holberton School students.
+ *@format: positive var
  * Return: Always 0.
  */
 void print_all(const char * const format, ...)
@@ -14,38 +13,37 @@ void print_all(const char * const format, ...)
 	va_list lst;
 
 	va_start(lst, format);
-
 	while (format[cont] != '\0')
 		cont++;
 
 	if (format[i] == '\0')
-	{
 		printf("(nil)");
-	}
 	else
 	{
 		while (format[i] != '\0')
 		{
 			switch (format[i])
 			{
-				case 'c': printf("%c", va_arg(lst, int));
+				case 'c':
+				printf("%c", va_arg(lst, int));
 				break;
-				case 'i': printf("%i", va_arg(lst, int));
+				case 'i':
+				printf("%i", va_arg(lst, int));
 				break;
-				case 'd': printf("%f", (float)(va_arg(lst, double)));
+				case 'd':
+				printf("%f", (float)(va_arg(lst, double)));
 				break;
-				case 's': printf("%s", va_arg(lst, char*));
-				break;
-				default: 
+				case 's':
+				printf("%s", va_arg(lst, char*));
 				break;
 			}
 			if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
- format[i] == 'd' || format[i] == 's'))
-			{
+format[i] == 'd' || format[i] == 's'))
 				printf(", ");
-			}
+
 			i++;
 		}
 		printf("\n");
 	}
+	va_end(lst);
 }
