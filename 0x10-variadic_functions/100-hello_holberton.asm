31be198351b052f2c56dca70a7f17b3ccc43ec35
towasme <towasm@gmail.com>
2019-10-28 04:36:16 +0000

    corrections

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,15 @@
-global _start
+section .data
+	text db "Hello, Holberton\n",10,0
 
 section .text
-
+	global main
 _start:
   mov rax, 1        ; write(
   mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  mov rsi, text      ;   "Hello, world!\n",
+  mov rdx, 17   ;   sizeof("Hello, world!\n")
   syscall           ; );
 
   mov rax, 60       ; exit(
   mov rdi, 0        ;   EXIT_SUCCESS
   syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton\n", 10
-  msglen: equ $ - msg
