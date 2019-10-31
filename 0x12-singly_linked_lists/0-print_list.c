df5a650944cb564cb4ffeb6a279281697338eb57
towasme <towasm@gmail.com>
2019-10-31 23:56:45 +0000

    fixed counter when its NULL and added new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,18 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 size_t print_list(const list_t *h)
-{;
+{
 	size_t counter = 0;
 
 	if (h->str == NULL)
 	{
-		printf("[0] (nil)");
+		printf("[0] (nil)\n");
 		h = h->next;
+		counter++;
 	}
 	while (h != NULL)
 	{
-		printf("[%d] %s", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		counter++;
 	}
