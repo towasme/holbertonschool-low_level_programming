c6f34e18c8cd8be5acf063d1d9ffe42a8d649927
towasme <towasm@gmail.com>
2019-10-31 23:44:02 +0000

    ans

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <list.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *print_list - prints a list
+ * @h: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+size_t print_list(const list_t *h)
+{;
+	size_t counter = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)");
+		h = h->next;
+	}
+	while (h != NULL)
+	{
+		printf("[%d] %s", h->len, h->str);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
