d79051454185bc81d0c1aae3a002fc61e451274e
towasme <towasm@gmail.com>
2019-11-01 03:21:01 +0000

    ans

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+**add_node - adds a new node to thelist
+*@head: list name
+*@str: string to add
+*Return: On success 1.
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int count = 0, ch = 0;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (head == NULL)
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	while (str[count])
+	{
+		count++;
+		ch++;
+	}
+	new->len = ch;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
