38da3bf2a3b06615744de7ac31544307021dc473
towasme <towasm@gmail.com>
2019-11-01 00:09:57 +0000

    removed conditionall if NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,24 +5,26 @@
 
 /**
  *print_list - prints a list
- * @h: The character to print
- * Return: On success 1.
+ *@h: The character to print
+ *Return: On success 1.
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		h = h->next;
-		counter++;
-	}
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
 		counter++;
 	}
 	return (counter);
