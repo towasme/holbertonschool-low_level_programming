77822e6279f431c5aed0c7eabfd7a92f8dcb7e8c
towasme <towasm@gmail.com>
2019-11-01 00:02:47 +0000

    corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,8 +7,8 @@
  *print_list - prints a list
  * @h: The character to print
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
