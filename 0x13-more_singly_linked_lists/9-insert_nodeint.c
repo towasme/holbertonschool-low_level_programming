783b854ed1b24e81122254ef8d5e06315afbde4b
towasme <towasm@gmail.com>
2019-11-06 05:27:23 +0000

    added null condtional for second var

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,10 +38,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		i++;
 		aux = aux->next;
+		if (aux == NULL)
+		{
+			return (NULL);
+		}
 	}
-	if (aux == NULL)
-		return (NULL);
-
 	new->next = aux->next;
 	aux->next = new;
 	return (new);
