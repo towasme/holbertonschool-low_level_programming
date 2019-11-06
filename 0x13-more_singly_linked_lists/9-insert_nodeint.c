1f39a7d50dedf945215324daa342283b6699e10b
towasme <towasm@gmail.com>
2019-11-06 04:35:04 +0000

    added conditionals

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
+		new->n = n;
 		return (new);
 	}
 	else if (*head == NULL || new == NULL)
