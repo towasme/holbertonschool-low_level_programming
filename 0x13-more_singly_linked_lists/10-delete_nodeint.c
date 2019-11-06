3730933881fe5d00517ecd6c7bf36d0c3e921a5d
towasme <towasm@gmail.com>
2019-11-06 05:07:57 +0000

    ans

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+*delete_nodeint_at_index - adds a new node to thelist
+*@head: list name
+*@index: number
+*Return: On success 1.
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *new, *aux;
+	unsigned int i = 1;
+
+	if (*head == NULL)
+		return (-1);
+
+	new = *head;
+	aux = *head;
+	if (index == 0)
+	{
+		if (new == NULL)
+			return (-1);
+
+		*head = (*head)->next;
+		free(new);
+		return (1);
+	}
+	while (i < index && new != NULL)
+	{
+		i++;
+		new = new->next;
+	}
+
+	aux = new->next;
+	new->next = aux->next;
+	free(aux);
+	return (1);
+}
