e5dac770192dc7d77098c808a95f9cb428c8ad68
towasme <towasm@gmail.com>
2019-11-07 20:28:47 +0000

    README files and header files

---
+++ 0x14-bit_manipulation/_pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_pow_recursion - prints characters
+ *@x: var
+ *@y: var 2
+ *Return: Always
+ */
+int _pow_recursion(int x, int y)
+{
+
+	if (y > 0)
+		{
+		return (x * _pow_recursion(x, (y - 1)));
+		}
+	if (y < 0)
+		{
+		return (-1);
+		}
+	else
+		{
+		return (1);
+		}
+}
