b59dc94685ae1b500725596715bcb418cd9e1391
towasme <towasm@gmail.com>
2019-11-07 22:20:14 +0000

    start

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *get_bit - prints a list
+ *@n: The character to print
+ *@index: unsignet int
+ *Return: On success 1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n >> index) & 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
