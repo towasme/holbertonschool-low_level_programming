0a7bcdc520d89db6835e6f1ebcacd404b8d221f1
towasme <towasm@gmail.com>
2019-11-08 05:15:16 +0000

    start

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *clear_bit - prints a list
+ *@n: The character to print
+ *@index: unsignet int
+ *Return: On success 1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if ((sizeof(unsigned long int) * 8) < index)
+	{
+		return (-1);
+	}
+	else
+	{
+		*n = ~*n | (1 << index);
+		return (1);
+	}
+}
