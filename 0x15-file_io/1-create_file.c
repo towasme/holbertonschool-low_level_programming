ccda94ce88ae120e113206eb6f3a4c2ae7f9d579
towasme <towasm@gmail.com>
2019-11-12 22:47:42 +0000

    what if fd returns error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (!fd)
+	if (fd == -1)
 	{
 		return (-1);
 	}
