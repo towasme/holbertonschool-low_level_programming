e3d599fda055ed23d9aea683996953b5d6c6dde7
towasme <towasm@gmail.com>
2019-11-12 22:37:57 +0000

    ans

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int _strlen(char *s);
+/**
+ *_strlen- Prints
+ *@s: var
+ *Return: Always
+ */
+int _strlen(char *s)
+{
+int n;
+
+	for (n = 0; s[n]; n++)
+		{
+		}
+	return (n);
+}
+
+
+/**
+ *create_file - program that reads a file
+ *@filename: The character to print
+ *@text_content: number of letters it prints
+ *Return: On success 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (!fd)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		write(fd, text_content, _strlen(text_content));
+	}
+	else
+	{
+	return (1);
+	}
+	close(fd);
+	return (1);
+}
