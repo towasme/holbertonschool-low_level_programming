13893f7c81a0b4ef82567ce8ed922122a6b3fdb8
towasme <towasm@gmail.com>
2019-11-12 22:45:05 +0000

    added new condition if text is empty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,13 +42,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (!text_content)
+	{
+		return (1);
+	}
 	if (text_content)
 	{
 		write(fd, text_content, _strlen(text_content));
-	}
-	else
-	{
-	return (1);
 	}
 	close(fd);
 	return (1);
