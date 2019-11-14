3588329dfddd748ea1ed91617937bf4b300cf96d
towasme <towasm@gmail.com>
2019-11-14 03:12:49 +0000

    added new if read output failled

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,20 +16,25 @@
 int main(int ac, char *av[])
 {
 	char buff[1024];
-	int fd, fa, fb;
-	size_t n_b;
+	int fd, fa, fb, n_b;
 
 	if (ac != 3)
-	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97); }
-	fd = open(av[1], O_RDONLY);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(av[1], O_RDONLY, 0600);
 	if (fd == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98); }
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	fa = open(av[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fa == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99); }
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	while ((n_b = read(fd, buff, 1024)) > 0)
 	{
 		fb = write(fa, buff, n_b);
@@ -39,12 +44,17 @@ int main(int ac, char *av[])
 			exit(99);
 		}
 	}
-	if (fb == -1)
+	if (n_b == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	if (fa == -1)
+	if (close(fa) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fa);
 		exit(100);
