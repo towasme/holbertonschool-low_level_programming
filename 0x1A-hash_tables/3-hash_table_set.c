6916004f283825c3d44d5aedf15c1a7a3a41d745
towasme <towasm@gmail.com>
2020-01-18 17:13:48 -0500

    func adds a new node at an specific index in the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ *hash_table_set - adds a element to the hash table
+ *@ht: is the hash table you want to add or update the key/value to
+ *@key: the key given by the function djb2
+ *@value: the value associated with the key
+ *Return: Always 0.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+	unsigned long int idx = 0;
+	char *new_value;
+
+	new_value = strdup(value);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = new_value;
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx] == NULL)
+	{
+		ht->array[idx] = new_node;
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = ht->array[idx];
+		ht->array[idx] = new_node;
+	}
+	return (1);
+}
---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "hetairas", "holberton");
+    hash_table_set(ht, "mentioner", "holberton2");
+    return (EXIT_SUCCESS);
+}
