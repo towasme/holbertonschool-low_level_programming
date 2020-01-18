754135f9f1114d2ee4349e871338e92731d29edd
towasme <towasm@gmail.com>
2020-01-18 14:08:08 -0500

    fucntion that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash.tables.h"
+#include <stdio.h>
+
+/**
+ *hash_table_t - creates a hash table
+ *@size: size of the hash table array
+ *Return: Always 0.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	new_table = NULL;
+
+	if (size == NULL)
+		return (NULL);
+	
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_table->array = calloc(size, sizeof(char *))
+			if (new_table->array == NULL)
+			{
+				free(new_table);
+				return (NULL);
+			}
+		new_table->size = size;
+	}
+	return (new_table);
+}
