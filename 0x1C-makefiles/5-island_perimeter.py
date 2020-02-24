6c9fc8ba3f0e91ec3248d5d62218c70837d6344d
towasme <towasm@gmail.com>
2020-02-24 04:56:33 +0000

    staritn with the advanced task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+find the perimeter of an island made of 1
+"""
+
+
+def island_perimeter(grid):
+    """
+    function
+    """
+    if grid:
+        count = 0
+        for row in grid:
+            for elem in row:
+                if elem == 1:
+                    count += 1
+                    print("1 {}".format(count))
+
+#           UP
+                if 
+#           LEFT
+
+#           DOWN
+
+#           RIGHT
+
+    return cont
