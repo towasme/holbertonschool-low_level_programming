96c28f8f443492809a5044fb3c8def1d4503b286
towasme <towasm@gmail.com>
2020-04-16 23:35:52 -0500

    modified binary code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,40 +10,29 @@
 int binary_search(int *array, size_t size, int value)
 {
 	unsigned int left = 0, right = (size - 1), floor = 0, i = 0;
-	unsigned int limit = (size - 1);
 
 	if (array)
 	{
-		while (left < right)
+		while (left <= right)
 		{
 			printf("Searching in array: ");
-			for (; i <= limit; i++)
+			for (i = (left); i < right; i++)
 			{
-				if (i == limit)
-					printf("%d\n", array[i]);
-				else
-				{
-					printf("%d, ", array[i]);
-				}
+				printf("%d, ", array[i]);
 			}
-			floor = ((left + right - 1) / 2);
+			printf("%d\n", array[i]);
+			floor = ((left + right) / 2);
 			if (array[floor] < value)
 			{
 				left = (floor + 1);
-				i = left + 1;
-				limit = right;
 			}
 			else if (array[floor] > value)
 			{
-				right = floor;
-				i = left;
-				limit = right;
+				right = floor - 1;
 			}
 			else
-			{
 				return (floor);
-			}
 		}
 	}
-	return (-1);
+return (-1);
 }
